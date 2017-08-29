d0222dfcf89cd9add1e8db85cf20de8c0ac75515
mestrada1 <31257576+mestrada1@users.noreply.github.com>
2017-08-29 14:05:36 -0500

    Add files via upload

--- Inclass2.m
+++ Inclass2.m
@@ -4,13 +4,25 @@
 % only the odd numbers
 
 for ii = 1:20
+    if mod(ii,2)==1
+    disp(ii)
+    end
 end
 
 % b. Write a new loop to display the same odd numbers which doesn't use a conditional statement 
 
+ii=1:20;
+odd=ii(mod(ii,2)==1);
+for n=1:10;
+    z(n) = odd(n)
+end
+
 % c. display the same odd numbers without a loop or conditional (hint use
 %  num2str)
 
+ii=1:20;
+odd=ii(mod(ii,2)==1);
+num2str(odd)
 
 %% 2. Vectorize the following code (that is produce the same result in z
 %without using a loop). 
@@ -21,20 +33,39 @@ for ii = 1:100
     z(ii) = ii*ii;
 end
 
+% % Vectorized code
+ii=1:100;
+y=ii.*ii
+
 %b. 
 z = 0;
 for ii = 1:100
     z = z + ii;
 end
 
+% % Vectorized code
+ii=1:100
+sum(ii)
+
+ans =
+
+        5050
+
 %c.
 xx = rand(50,2);
 
 for ii = 1:50
-    z(ii) = sqrt(xx(ii,1)^2+xx(ii,x)^2);
+    z(ii) = sqrt(xx(ii,1)^2+xx(ii,2)^2);
 end
 
+% % Vectorized code 
+
+z = sqrt(sum(xx.*xx, 2));
+
 % d. what is the formula in c computing?
+
+% The formula in c is computing the distance of points in the xx matrix
+% from an origin. 
 
 %% plotting
 
@@ -42,14 +73,41 @@ end
 %of sin(x) vs x is a red-dashed line and the graph of cos(x) vs x is a
 %blue solid line.
 
+% % Plot code
+x=0:4*pi;
+plot(x,sin(x), 'r--', x, cos(x), 'b-')
+
+% % see attached figure1_sin(x)_cos(x)
+
 %b. repeat the above but where each graph is in a different subplot in the
 % same figure. 
 
+% % Plot code
+x=0:4*pi;
+subplot(1,2,1);
+plot(x, sin(x), 'r--');
+subplot(1,2,2);
+plot(x, cos(x), 'b-');
+
+% % see attached figure2_sin(x)_cos(x)
+
 %c. here is some data, imagine they are x and y coordinates for cells. 
 xy_dat = rand(100,2);
+
 %plot the data in the xy plane.
+
+% % Plot code
+scatter(xy_dat(:,1), xy_dat(:,2))
+
+% % see attached scatter_xy_dat
+
 %now imagine there is some fluorescence data corresponding to the same
 %cells
 fluor = rand(100,1);
 % plot the data in the xy plane but color code the data by the fluorescnece
 % values. Hint: see the scatter command
+
+% % Plot code
+scatter(xy_dat(:,1), xy_dat(:,2), 24, fluor, 'filled')
+
+% % see attached scatter_fluor
